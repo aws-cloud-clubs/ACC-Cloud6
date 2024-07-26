f119766ba82b12a52a9125ff12b449c5e3baf736
wingyou <winwing.you@gmail.com>
2024-07-27 05:03:49 +0900

    feat: radis setup

---
+++ match/src/main/java/com/cloud6/match/RedisTestController.java
@@ -0,0 +1,28 @@
+package com.cloud6.match;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class RedisTestController {
+
+    private final StringRedisTemplate template;
+
+    @GetMapping("/test")
+    public String getTest() {
+        return template.opsForValue().get("test");
+    }
+
+
+    @PostMapping("/test")
+    public void setTest(@RequestParam String value) {
+        template.opsForValue().set("test", value);
+    }
+}
+
